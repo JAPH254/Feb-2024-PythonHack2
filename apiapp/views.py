46fa97aa28e5bc2bfb9b71d0aae1ce17f805a285
JAPH254 <omanyujapheth@gmail.com>
2024-04-28 16:08:51 +0300

    complete setup of the two apps

--- apiapp/views.py
+++ apiapp/views.py
@@ -5,28 +5,19 @@ import os
 from . models import user
 from .forms import CustomUserCreationForm
 def home(request):
-    # Retrieve location from user input (replace with appropriate method)
     location = request.GET.get('location')
-
-    # Validate location input (optional, but recommended for robustness)
     if not location:
         context = {'error': 'Please enter a valid location.'}
         return render(request, 'templates/home.html', context)
-
-    # Construct the API URL with a placeholder for the location
     url = 'https://weatherapi-com.p.rapidapi.com/current.json?q={}'
-
-    # Set headers with your X-RapidAPI-Key (avoid exposing it in code)
     headers = {
         'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
         'X-RapidAPI-Key': 'c91f9666e1msha2a78962a9ff863p1255ffjsnc55172833efb'
-        # os.environ.get('YOUR_API_KEY')  # Access from environment variable
     }
 
     try:
-        # Make the API request, handling potential errors gracefully
         response = requests.get(url.format(location), headers=headers)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
+        response.raise_for_status()
 
         data = response.json()
         context = {'data': data}
